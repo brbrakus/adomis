8aa2a09a8a9bdd45740dee544cec88991168c574
Miro Dojkic <mdojkic@gmail.com>
2017-07-06 01:02:30 +0200

    Bundle js

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+import uglify from 'rollup-plugin-uglify'
+import buble from 'rollup-plugin-buble'
+
+export default {
+    entry: 'js/index.js',
+    dest: 'public/bundle.js',
+    format: 'iife',
+    plugins: [
+        buble(),
+        uglify()
+    ]
+}
