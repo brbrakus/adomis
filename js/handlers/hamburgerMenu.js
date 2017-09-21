9e9ea9ed23690ecedd72a534a030842f7c0ae7ac
Miro Dojkic <mdojkic@gmail.com>
2017-09-21 14:52:40 +0200

    hamburger, forEach polyfill, precommit hook

---
+++ js/handlers/hamburgerMenu.js
@@ -0,0 +1,14 @@
+export default () => {
+  const hamburgerMenu = document.querySelector('header .hamburger-menu')
+  const navbarItems = document.querySelector('header .navbar-items')
+
+  hamburgerMenu.addEventListener('click', () => {
+    navbarItems.classList.toggle('active')
+  })
+
+  navbarItems.children.forEach(navbarItem =>
+    navbarItem.addEventListener('click', () => {
+      navbarItems.classList.toggle('active')
+    })
+  )
+}
