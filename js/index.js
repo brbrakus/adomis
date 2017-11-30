2dbfbd4ac9365b34f37386aa6a4762321a9de6ac
Miro Dojkic <mdojkic@gmail.com>
2017-11-30 13:03:42 +0100

    optimizations

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 import polyfill from './polyfill'
 import loadImages from './common/loadImages'
+import loadIframes from './common/loadIframes'
 import hamburgerMenu from './handlers/hamburgerMenu'
 
 document.addEventListener('DOMContentLoaded', () => {
   polyfill()
   loadImages()
+  loadIframes()
   hamburgerMenu()
 })
